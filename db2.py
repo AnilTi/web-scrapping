66012d8f246e2f1db0f611858482a08b634d25d2
AnilTi <94893827+AnilTi@users.noreply.github.com>
2022-06-10 10:18:44 +0530

    Add files via upload

---
+++ db2.py
@@ -0,0 +1,22 @@
+## How to create table of selected database
+
+import pymysql
+conn=pymysql.connect(host='localhost',user='root',password='',db='demo2')
+
+a=conn.cursor()
+
+a.execute('''CREATE TABLE COMPANY
+       (ID INT PRIMARY KEY  NOT NULL,
+        NAME TEXT NOT NULL,
+        AGE INT  NOT NULL,
+        ADDRESS CHAR(50),
+        SALARY  REAL)''')
+
+print("Table created successfully")
+
+conn.close()
+
+import pymysql
+conn.pymysql.connect(host='localhost',user="root",password='',db = 'practice2')
+a = conn.cursor()
+a.execute
